bdf9e2094568b0910c8cdb9dad17d6e066891bf1
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-11-26 12:39:59 +0600

    Update QRCode_gen.py

--- QRCode_gen.py
+++ QRCode_gen.py
@@ -1,12 +1,18 @@
+#import dependencies
 import pyqrcode
 import png
 
+#text which you want to convert in QRCode
 QRtext = input(print('Enter text to generate QR: '))
 
+#filename that will be saved
 QRimg = input(print('Enter image name to save: '))
 
+#image extension
 QRimg = QRimg + '.png'
 
+
+#Generating Final QRCOODE
 FinalQR = pyqrcode.create(QRtext)
 
 FinalQR.show()
